2809e337495f578dc4d8acde65bd7d883dbff2b0
Aiden Lee <aiden.leews@gmail.com>
2022-01-27 15:31:08 -0500

    Update number_guesser.py
    - "try & except" to restrict user input since isdigit doesn't work for negative integers.
    - "continue" instead of "quit()" to keep the game going.

--- number_guesser.py
+++ number_guesser.py
@@ -1,35 +1,40 @@
 import random
 
-top_of_range = input("Type a number: ")
+# User input restriction to positive integers
+while True:
+    top_of_range = input('Give me a number: ')
+    try:
+        top_of_range = int(top_of_range)
+        if top_of_range > 0:
+            print("You have to guess a number between 0 -", top_of_range)
+            break  # Getting out of the loop for the main game
+        else:
+            print('Give me a number bigger than 0.')
+            continue
+    except ValueError:
+        print('Number only please.')  # User input is not an integer
+        continue
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
-
-    if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
-        quit()
-else:
-    print('Please type a number next time.')
-    quit()
-
+# Main game
 random_number = random.randint(0, top_of_range)
 guesses = 0
 
 while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
+    user_guess = input('Your guess: ')
+    try:
         user_guess = int(user_guess)
-    else:
-        print('Please type a number next time.')
+        guesses += 1
+        if user_guess == random_number:
+            print('You got it!')
+            break
+        elif user_guess < random_number:
+            print('Above')
+            continue
+        else:
+            print('Below')
+            continue
+    except ValueError:
+        print('Number only please.')  # Not an integer
         continue
 
-    if user_guess == random_number:
-        print("You got it!")
-        break
-    elif user_guess > random_number:
-        print("You were above the number!")
-    else:
-        print("You were below the number!")
-
-print("You got it in", guesses, "guesses")
+print("You got it in", guesses, "guess(es).")
